b2a4e502da7c71883c1610bafbf9e61f0d838d78
LuisDxl98 <6228@itescam.edu.mx>
2024-05-11 14:24:16 -0600

    Nuevo campo de Credito

---
+++ example_controller/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Example Controller',
+    'version': '1.0',
+    'description': 'Ejemplo de controllador para POS',
+    'license': 'LGPL-3',
+    'category': 'Pos',
+    'depends': [
+        'base','point_of_sale'
+    ],
+    'data': [],
+    'auto_install': False,
+    'application': False
+}
