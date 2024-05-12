10634378d057d354e0ad971ff7c076a9ef4c7b66
LuisDxl98 <6228@itescam.edu.mx>
2024-05-12 01:09:37 -0600

    Update local

--- example_controller/models/res_partner.py
+++ example_controller/models/res_partner.py
@@ -4,8 +4,8 @@ from odoo import models, fields, api
 class ResPartnerCustomFields(models.AbstractModel):
     _inherit = 'res.partner'
 
-    vendor_id = fields.Integer(related='create_uid.id', string="ID del vendedor")
-    vendor_name = fields.Char(related='create_uid.name', string="Nombre del vendedor")
+    vendor_id = fields.Integer(related='user_id.id', string="ID del vendedor")
+    vendor_name = fields.Char(related='user_id.name', string="Nombre del vendedor")
     superuser_ids = fields.Many2many("res.users", compute='_compute_res_users_admins', store=True )
     
     @api.depends('name')
